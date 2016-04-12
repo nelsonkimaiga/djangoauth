4071ad694ab5f24dd82bd07d4e9e79c6ebc9460b
nelsonkimaiga <nelsonkimaiga@aol.com>
2016-04-12 12:15:29 +0300

    mapped urls

--- untitled/urls.py
+++ untitled/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+urlpatterns = patterns['',
+    :
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    :
 ]
